406737af8d68605d57ba7e57be943d7a63a26c10
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-01-03 16:44:16 +0100

    [IMP] - Mobile Changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
+// Header Scroll
+const header = document.querySelector('#step1');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('header-white');
+  } else {
+    header.classList.remove('header-white');
+  }
+});
+
+if (window.scrollY > 50) {
+  header.classList.add('header-white');
+} else {
+  header.classList.remove('header-white');
+}
+
 // Step 3 Text Expansion
 const step3Btns = document.querySelectorAll('#step3 button');
+const step3Li = document.querySelectorAll('#step3 ul li');
+
+if (window.innerWidth > 1024) {
+  step3Li.forEach((li) => {
+    li.classList.add('show');
+  });
+}
 
 step3Btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
